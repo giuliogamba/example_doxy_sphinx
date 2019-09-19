8b4f5c4f5ece1b11975c2c46e463daf73c555b22
Giulio <giuliog@xilinx.com>
2019-09-19 12:18:42 -0400

    Added doxygen to conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,13 @@
 import sys
 import datetime
 # sys.path.insert(0, os.path.abspath('.'))
+import subprocess, os
 
+read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+
+if read_the_docs_build:
+
+    subprocess.call('cd ../doxygen; doxygen Doxyfile', shell=True)
 
 # -- Project information -----------------------------------------------------
 
